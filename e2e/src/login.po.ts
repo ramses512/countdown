1e368d928c5975b2c1630b6d827f4b10f5937193
Ramses Fernández <aramsesfernandez@gmail.com>
2020-11-20 17:07:48 +0100

    fix: remove e2e

--- e2e/src/login.po.ts
+++
@@ -1,6 +0,0 @@
-import { by, element, ElementFinder } from 'protractor';
-export class LoginPage {
-  public getMessageError(): ElementFinder {
-    return element(by.css('.mat-simple-snackbar  > span'));
-  }
-}
--- src/app/data/schema/transaction.ts
+++
@@ -1,8 +0,0 @@
-export class Transaction {
-  id: number;
-  date: string;
-  amount: number;
-  fee: number;
-  description: string;
-  userId: number;
-}
